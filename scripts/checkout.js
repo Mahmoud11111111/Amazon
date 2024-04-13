efa0ec1fdfa333da587e38a8c33d0d0fbc7a8453
Mahmoud El Gharamti <mgharamti@yahoo.com>
2024-04-13 21:06:04 +0200

    Make delivery options interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart } from '../data/cart.js';
+import { cart, removeFromCart, updateDeliveryOption } from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 // external library (dayjs ESM) to put date 
@@ -98,7 +98,7 @@ function deliveryOptionsHTML(matchingProduct, cartItem){
       const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
    html += `
-        <div class="delivery-option">
+        <div class="delivery-option js-delivery-option" data-product-id="${matchingProduct.id}" data-delivery-option-id="${deliveryOption.id}">
         <input type="radio"
         ${isChecked ? 'checked' : ''}
           class="delivery-option-input"
@@ -128,4 +128,13 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
    container.remove();
   });
 });
+
+document.querySelectorAll('.js-delivery-option').forEach((element)=>{
+  element.addEventListener('click',()=>{
+    // const productId = element.dataset.productId;
+    // const deliveryOptionId = element.dataset.deliveryOptionId;
+    const {productId, deliveryOptionId} = element.dataset;
+    updateDeliveryOption(productId, deliveryOptionId);
+  })
+})
 
